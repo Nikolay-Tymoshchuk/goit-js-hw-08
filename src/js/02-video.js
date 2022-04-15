211b6cf4463a000f88e66679e6e1a22e234f1ecc
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-04-15 15:00:28 +0300

    inicialize vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+{/* <script src="https://player.vimeo.com/api/player.js"></script> */}
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
